ecdc32f2dfbcc2f4efa06a12870825590e0f879c
pc4 <amirrezasardari@outlook.com>
2023-05-29 23:55:47 -0700

    Second Commit Poortaghi

---
+++ app/src/main/java/com/example/pc4/Destonations.kt
@@ -0,0 +1,16 @@
+package com.example.pc4
+
+
+interface Destonations
+{
+    val Route :String
+}
+
+object Home :Destonations
+{
+    override val Route ="Home"
+}
+object Second:Destonations
+{
+    override val Route ="Second"
+}
